ef133b399c02f208763e02abc704b012a844b521
gokul3062 <gokularasan3062@gmail.com>
2025-04-21 10:30:08 +0530

    commit

--- src/YieldAsGenerator.py
+++ src/YieldAsGenerator.py
@@ -35,3 +35,19 @@ end = time.time()
 
 print(f"Time Taken without Yield {end - start:.2f}")
 
+
+#Using Next in generator
+generator = fn()
+for _ in range(5):
+    val = next(generator)
+    print(val)
+    
+
+#Can be used in Memory Effecient Tasks
+# def read_lines(filename):
+#     with open(filename) as f:
+#         for line in f:
+#             yield line.strip()
+
+# for line in read_lines("big_file.txt"):
+#     process(line)  # Processes line-by-line, memory safe!
